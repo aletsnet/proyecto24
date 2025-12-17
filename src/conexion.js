190b6488e6ca3b7ff1fa148156855c8a574c51e6
victor <victorhugo180505@gmail.com>
2025-12-17 16:56:36 -0600

    Modificacion a conexion

---
+++ src/conexion.js
@@ -0,0 +1,17 @@
+let mysql = require('mysql2');
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    database: '',
+    user: 'root',
+    password: ''
+});
+
+conexion.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+    else {
+        console.log("Conexion exitosa");
+    }
+
+});
