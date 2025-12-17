f9af76b463f63a356737bc7df4570e1bcbec1415
victor <victorhugo180505@gmail.com>
2025-12-17 17:30:38 -0600

    no se

--- src/conexion.js
+++ src/conexion.js
@@ -1,7 +1,7 @@
 let mysql = require('mysql2');
 let conexion = mysql.createConnection({
     host: 'localhost',
-    database: '',
+    database: 'proyecto24',
     user: 'root',
     password: ''
 });
