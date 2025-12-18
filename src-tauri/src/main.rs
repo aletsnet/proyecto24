fc3fa3c0bd7d584309c4ec2bfe556c75c086839e
Alejandro Lopez Ortiz <Alejandro.bin@gmail.com>
2025-12-18 01:31:33 -0600

    Basico

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri_app_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_opener::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
---
+++ src/js/views.js
@@ -0,0 +1,18 @@
+//funcion que renderiza las vistas
+export const renderView = async (dom, page) => {
+    const app = document.getElementById(dom);
+    app.innerHTML = ''; // Limpiar el contenido previo
+    let viewContent = '';
+    
+    await fetch('/src/' + page + '.html')
+        .then(response => response.text())
+        .then(html => {
+            app.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error al cargar la vista:', error);
+            app.innerHTML = '<h1>Error al cargar la vista</h1>';
+        });
+            
+    app.innerHTML = viewContent;
+}
